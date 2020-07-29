770a13e4c0779b06008f3220b3194c5b54245bad
jainatsaurabh <31925369+jainatsaurabh@users.noreply.github.com>
2020-07-29 08:02:34 +0530

    Add files via upload

--- src/main/java/com/example/virus/VirusApplication.java
+++ src/main/java/com/example/virus/VirusApplication.java
@@ -2,15 +2,20 @@ package com.example.virus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
-@SpringBootApplication(scanBasePackages = "com.example.virus")
+@SpringBootApplication
+@ComponentScan(basePackages = "com.example.virus")
 @EnableJpaRepositories("com.example.virus.repo")
 public class VirusApplication {
-	static {
-
-		System.out.println("VirusApplication............................................");
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VirusApplication.class, args);
